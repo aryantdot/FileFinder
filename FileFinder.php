1b9683522fbcc953c2080e19cf6938ffb59f16b5
Aryantdot <69663102+aryantdot@users.noreply.github.com>
2021-04-14 18:51:09 +0430

    Add files via upload

---
+++ FileFinder.php
@@ -0,0 +1,30 @@
+<?php
+
+// PHP 8.0
+// Aryantdot
+
+$__files = [];
+function localSearch($file , $dir){
+    global $__files;
+    $total = 0;
+    switch(is_dir($dir)):
+        case true:
+            foreach(glob($dir.'/*') as $tag):
+                if(is_dir($tag)){
+                    $e = localSearch($file , $tag);
+                    $total += $e;
+                }else{
+                    if(strpos(basename($tag), $file) !== false){
+                        $__files[++$total] = $tag;
+                    }
+                }
+            endforeach;
+            return $total;
+        case false:
+        default:
+            throw new Exception('Folder not found :: 404');
+    endswitch;
+}
+
+echo localSearch('ase', '.');
+print_r($__files);
